9ac574ff80933b765d921cb2af5aa99514343fbe
Qamas95 <kamil.p.kubiak@gmail.com>
2022-01-23 21:20:53 +0100

    Clean comments, find price by google

--- stock/scrap.py
+++ stock/scrap.py
@@ -1,5 +1,7 @@
+from http import server
 from lib2to3.pgen2 import driver
 from locale import currency
+from multiprocessing.sharedctypes import Value
 from re import search
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -18,13 +20,11 @@ chosen_currency = ['EUR', 'USD', 'PLN']
 
 
 def search_price():
-    wait = WebDriverWait(driver, 10)
-    driver.get('https://www.google.com/search?q=' + financial_asset[0] + chosen_currency[0] + 'price')
-    # def get_price():
-    #     return driver.find_element_by_xpath('/html/body/div[7]/div/div[10]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div/div/div/div[2]/span[1]').text
-
-    # print(get_price())
-
+        wait = WebDriverWait(driver, 10)
+        driver.get('https://www.google.com/search?q=' + financial_asset[0] + chosen_currency[0] + 'price')
+        driver.find_element_by_xpath('/html/body/div[3]/div[3]/span/div/div/div/div[3]/button[2]/div').click()
+        value = driver.find_element_by_class_name('pclqee').text
+        print(value)
 
 search_price()
 
@@ -33,34 +33,3 @@ driver.quit()
 
 
 
-# # os.environ['PATH'] += r"C:/Users/kamil/OneDrive/Pulpit/Stock"
-
-# os.environ['PATH'] += r"D:/Users/kk/Pulpit/Stock/Scrapper"
-
-# driver = webdriver.Chrome()
-
-# driver.get('https://www.londonstockexchange.com')
-
-# wait = WebDriverWait(driver, 10)
-
-# def search(ticker):
-#     wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'ccc-notify-buttons')))
-#     searchEL = driver.find_element_by_id('ccc-notify-accept')
-#     searchEL.send_keys(Keys.ENTER)
-#     searchEL = driver.find_element_by_id('quick-search-input')
-#     searchEL.send_keys(ticker)
-#     searchEL.send_keys(Keys.DOWN)
-#     searchEL.send_keys(Keys.ENTER)
-#     searchEL = driver.find_element_by_link_text('Explore more about this Instrument')
-#     searchEL.send_keys(Keys.ENTER)
-#     print(get_last_price())
-
-# def get_last_price():
-#     return driver.find_element_by_xpath('/html/body/app-root/app-handshake/div/app-page-content/app-tab-nav/app-company-ticker/div/div/section/div[1]/div[2]/span[1]/div/span[1]/span').text
-
-
-# search('UU')
-
-
-# time.sleep(300)
-# driver.quit()
